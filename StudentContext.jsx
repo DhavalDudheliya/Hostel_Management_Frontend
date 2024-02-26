cfb13d223b2f9f2033e35618c8a42b1ac2651842
Dhaval Dudheliya <dhavaldudheliya77@gmail.com>
2024-02-26 20:02:53 +0530

    due penalty table is compelted

--- StudentContext.jsx
+++ StudentContext.jsx
@@ -6,10 +6,11 @@ const StudentContext = createContext();
 export const StudentProvider = ({ children }) => {
   const [student, setStudent] = useState();
   const [subFees, setSubFees] = useState();
+  const [dueFees, setDueFees] = useState();
 
   return (
     <StudentContext.Provider
-      value={{ student, setStudent, subFees, setSubFees }}
+      value={{ student, setStudent, subFees, setSubFees, dueFees, setDueFees }}
     >
       {children}
     </StudentContext.Provider>
--- src/pages/Accountant/Fees/DueFees.jsx
+++ src/pages/Accountant/Fees/DueFees.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { UserContext } from "../../../../UserContext";
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
+import DueFeeCard from "@/components/Fees/DueFeeCard";
+import DueFeeTable from "@/components/Fees/DueFeeTable";
 
 function DueFees() {
   const { user, setUser } = useContext(UserContext);
@@ -10,7 +12,12 @@ function DueFees() {
     return <Navigate to="/login" />;
   }
 
-  return <div>DueFees</div>;
+  return (
+    <div className="p-6 lg:pl-6 lg:pr-10">
+      <DueFeeCard />
+      <DueFeeTable />
+    </div>
+  );
 }
 
 export default DueFees;
